34931d6bf93f4d0ec90ddf3f73299198e700a1dc
duanhyk <duanhyk@yonyou.com>
2018-03-26 14:03:19 +0800

    适配安卓

--- src/components/Echarts/index.js
+++ src/components/Echarts/index.js
@@ -5,22 +5,22 @@ import echarts from './echarts.min';
 
 export default class App extends Component {
   componentWillReceiveProps(nextProps) {
-    if(nextProps.option !== this.props.option) {
+    if (nextProps.option !== this.props.option) {
       this.refs.chart.reload();
     }
   }
 
   render() {
     return (
-      <View style={{flex: 1, height: this.props.height || 400,}}>
+      <View style={{ flex: 1, height: this.props.height || 400, }}>
         <WebView
           ref="chart"
-          scrollEnabled = {false}
-          injectedJavaScript = {renderChart(this.props)}
+          scrollEnabled={false}
+          injectedJavaScript={renderChart(this.props)}
           style={{
             height: this.props.height || 400,
           }}
-          source={require('./tpl.html')}
+          source={Platform.OS === 'android' ? { uri: 'file:///android_asset/tpl.html' } : require('./tpl.html')}
           onMessage={event => this.props.onPress ? this.props.onPress(JSON.parse(event.nativeEvent.data)) : null}
         />
       </View>
